3cb15e248db07036a43e9e4cd16c70aaf8587145
shobhit099 <shobhitsamaria7@gmail.com>
2021-03-26 22:57:09 +0530

    commit it

--- hello.py
+++ hello.py
@@ -2,26 +2,32 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from time import time
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_migrate import Migrate
 import os
 import re
+import bcrypt
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__ , static_folder = 'static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
 
-class users(db.Model):
+class users(UserMixin , db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(50))
+    hash = db.Column(db.String(50))
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -43,12 +49,17 @@ class Post(db.Model):
     def __repr__(self):
         return f'<Post id:{self.id}, title:{self.title}>'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
 @app.route('/home/<p>')
+@login_required
 def home(p):
     name = p
     posts = Post.query.filter(Post.authore.contains(p))
@@ -68,11 +79,13 @@ def login():
         password = request.form['password']
         user = users.query.filter_by(email = email).first()
         if user:
-            if user.password == password:
+            if bcrypt.checkpw(password.encode('utf-8') , user.hash ):
+                login_user(user)
                 return redirect(url_for('home',p = user.name))
     return render_template('login.html')
 
 @app.route('/blog1/<p>')
+@login_required
 def blog1(p):
     q = request.args.get('q')
 
@@ -116,11 +129,13 @@ def view1(slug):
     return render_template('view1.html',post=post)
 
 @app.route('/<p>/<slug>')
+@login_required
 def view(p,slug):
     post = Post.query.filter(Post.slug == slug).first()
     return render_template('view.html',post=post,p=p)
 
 @app.route('/write/<p>',methods=["POST","GET"])
+@login_required
 def write(p):
     if request.method == 'POST':
         title = request.form['title']
@@ -139,7 +154,8 @@ def sign_up():
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        user = users(name = name, email = email, password = password)
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user = users(name = name, email = email, hash = hashed)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('index'))
@@ -150,10 +166,12 @@ def contact():
     return render_template('contact_us.html')
 
 @app.route('/contact_us/<p>')
+@login_required
 def contact1(p):
     return render_template('contact_us1.html',p=p)
 
 @app.route('/about/<p>')
+@login_required
 def about1(p):
     return render_template('about1.html',p=p)
 
@@ -161,6 +179,11 @@ def about1(p):
 def about():
     return render_template('about.html')
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
